f1c74ce5f8e78e9c8e6b958e1c318c9cb933029b
Ruel Tingling <rvtingling@gmail.com>
2019-11-08 16:09:32 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,12 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
 
 class Dancer 
   
-  include  Dance 
-  
-  extend MetaDancing
+  #include  Dance 
+  #extend MetaDancing
   
   attr_accessor :name 
   
