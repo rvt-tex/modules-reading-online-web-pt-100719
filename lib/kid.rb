afd5431c9cf6eafe71eb0444ce1d7aeb69181c4f
Ruel Tingling <rvtingling@gmail.com>
2019-11-08 16:08:27 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,10 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './class_methods_module.rb'
+
 
 class Kid 
   
-  include  Dance 
-  extend MetaDancing
+  #include  Dance 
+  #extend MetaDancing
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   
   attr_accessor :name 
   
